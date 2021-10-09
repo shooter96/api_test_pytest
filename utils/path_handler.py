6e11f89353e73b62f109c8e071dd8f30ba33377b
wangy <wangy@isyscore.com>
2021-10-09 17:51:08 +0800

    通用方法

---
+++ utils/path_handler.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import os
+
+# one_path = os.path.abspath(__file__)
+# two_path = os.path.dirname(one_path)
+# three_path = os.path.dirname(two_path)
+# 项目根路径
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# 获取配置文件所在的路径
+CONFIGS_DIR = os.path.join(BASE_DIR, 'conf')
+
+# 获取配置文件所在的路径
+CONFIG_FILE_PATH = os.path.join(CONFIGS_DIR, 'testcase.yml')
+
+# 获取日志文件所在的目录路径
+LOGS_DIR = os.path.join(BASE_DIR, 'logs')
+
+# 获取报告文件所在的目录路径
+REPORTS_DIR = os.path.join(BASE_DIR, 'report')
+
